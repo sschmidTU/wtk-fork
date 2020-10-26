72cc80ed2e0cace56a6caf79795d48b66e154b31
sschmidTU <s.schmid@phonicscore.com>
2020-10-26 06:43:26 +0100

    add some wk radicals replacements

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,58 @@ $(function() {
 
   function search() {
     var query   = $('#search-query').val();
+    var wk_replacements = {
+      "net": "eye",
+      "stick": "walking stick",
+      "drop": "drop of",
+      "specialty": "acupuncturist",
+      "toe": "divining rod",
+      "morning": "mist",
+      "fins": "animal legs",
+      "legs": "human legs",
+      "table": "wind", //wind/weather vain
+      "prison": "bound up", //or bound up small
+      "horns": "horns",
+      "lion": "straightened hook",
+      "barb": "hook",
+      "ground fins": "tool", // not ideal, this needs to be in combination
+      "narwhal": "by ones side",
+      "knife": "saber",
+      "scarecrow": "wealth",
+      "window": "mama",
+      "triceratops": "little",
+      "cliff": "cliff",
+      "flood": "flood",
+      "river": "stream",
+      "tsunami": "water",
+      "original": "spring", // not perfect match, wk has cliff added
+      "boil": "fire",
+      "head": "hood",
+      "roof": "house",
+      "angel": "pole",
+      "flower": "flower",
+      "greenhouse": "graveyard",
+      "icicle": "turtle",
+      "animal": "pack of dogs",
+      "slide dirt": "cow",
+      "hat ground": "meeting",
+      "hat": "umbrella",
+      "scooter": "road",
+      "winter": "walking legs",
+      "death star": "convoy",
+      "forehead": "crown",
+      "lid table": "whirlwind",
+      "lid mouth": "tall",
+      "lid": "top hat",
+      "dirt mouth": "lidded crock",
+      "viking": "schoolhouse",
+      "brush": "brush",
+      "ceremony": "arrow"
+    }
+    for (const [key, value] of Object.entries(wk_replacements)) {
+      query = query.replace(key + " ", value + " ");
+      console.log("changed query: " + query);
+    }
     var result  = $('#search-results');
     var entries = $('#search-results .entries');
 
