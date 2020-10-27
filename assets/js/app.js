ddbc55b9d6cf6cc10832b83838024d4ad6c5eb35
sschmidTU <s.schmid@phonicscore.com>
2020-10-28 00:14:50 +0100

    add bear, spikes, pope wk radicals

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,7 +13,7 @@ $(function() {
     var query   = $('#search-query').val();
 
     if (query == "version") {
-      console.log("1.0.0.5");
+      console.log("1.0.0.6");
     }
 
     // mapping from WK radicals to RTK elements. (format of the values is comma separated, no spaces between values)
@@ -304,6 +304,9 @@ $(function() {
       // --------------------------------------------
       "elf": "daring",
       "coral": "helping hand",
+      "bear": "maestro without baton",
+      "spikes": "row",
+      "pope": "ten eye",
       "tofu": "rag", // actually exists in RTK, indirectly, description of 旅
       "stick": "walking stick",
       "small drop": "valentine",
