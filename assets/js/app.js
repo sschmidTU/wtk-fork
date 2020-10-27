fbe3a4f164f28e9b934a3972b06f34160b59c660
sschmidTU <s.schmid@phonicscore.com>
2020-10-27 02:31:57 +0100

    add many more replacements, up to p178

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,19 @@
 $(function() {
-	$('#search-button').on('click', function() {
+  $('#search-button').on('click', function() {
     return search();
   });
   
   $('#search-query').on('keyup', function() {
     return search();
   });
-
+  
   function search() {
     var query   = $('#search-query').val();
     var wk_replacements = {
       "cross": "ten",
       "moon": "month",
       "ricepaddy": "rice field",
+      "net": "eye", // own radical in WK, just horizontal eye in RTK
       "dare": "risk", // not a WK radical, WK: sun + eye
       "crystal": "sparkle",
       "products": "goods",
@@ -29,32 +30,112 @@ $(function() {
       "fortune": "fortune-telling",
       //"table": "eminent", // p33, collides with WK radical table and kanji table
       "morning": "mist", // p34
+      "table": "wind", //wind/weather vain
+      "prison": "bound up", //or bound up small
+      //"horns": "horns",
+      //"child": "newborn babe", // conflict: this is the kanji child in WK (former+legs), but there's also the RTK child radical
+      "shamisensong": "pop song",
+      "chastity": "upright",
+      "member": "employee",
+      "origin": "beginning",
+      "geoduck": "page",
+      "paragraph": "phrase", //p41
+      "season": "decameron", //p41
+      "pool": "ladle", //p42
+      //"neck": "neck",
+      "nose": "fish guts", //p44
+      "fix": "straight up",
+      "reality": "true", //p46
+      "narwhal": "by ones side",
+      "construction": "craft",
+      "have": "possess", //p47
+      // sword, blade, cut all same
+      "call": "seduce",
+      "scarecrow": "wealth", //p50
+      "town": "village", //p51
+      "lipring": "can",
+      "finish": "complete", //p53, only kanji in WK
+      "like": "fond", //p54, only kanji in WK
+      "mother": "mama", //p54, only kanji in WK
+      "olderbrother": "elder brother",
+      "small": "little",
+      "big": "large",
+      "sunlight": "ray", //p61
+      "fat": "plump",
+      "container": "utensil", //wk: kanji only
+      "strange": "exquisite", //wk: kanji only
+      "conserve": "focus", //wk: kanji only
+      "odd": "strange", //p63
+      "river": "stream",
+      "original": "meadow", //p67, or spring, without the cliff
+      "temple": "buddhist temple", //p75
+      "flame": "inflammation",
+      "head": "hood", //p83, or belt, p161
+      "roof": "house", //p85
+      "letter": "character", //only kanji in WK
+      "protect": "guard",
+      //"mutual": "inter", // conflict with other mutual kanji in WK
+      "omen": "portent", //p104
+      "nature": "sort of thing",
+      "announce": "revelation",
+      "previous": "before", //p108
+      "hat": "umbrella", //p109
+      "suit": "fit", //p110
+      "all": "whole", //p114, only kanji in WK
+      "reason": "logic", //only kanji in WK
+      "master": "lord",
+      "scooter": "road", //p122
+      "winter": "walking legs", //p125, or taskmaster, p137
+      "kiss": "each",
+      "forehead": "crown", //p128
+      "lid table": "whirlwind",
+      "lid mouth": "tall",
+      "lid": "top hat", //p130
+      "samurai": "gentleman", //p134
+      "viking": "schoolhouse",
+      "warn": "admonish",
+      "ceremony": "arrow", //p143
+      "drunkard": "fiesta",
+      "tocut": "thanksgiving", //p145, "to cut" is not from WK but from Jisho/Phonetic-semantic composition
+      "become": "turn into", //p146
       // ^ up until here checked with RTK physical edition, at least for WK radicals
-      "net": "eye", // own radical in WK, just horizontal eye in RTK
+      "bar": "float", //p148
+      "plan": "undertake", //p150, only kanji in WK
+      "coatrack": "mending", //p152, or zoo, p155
+      "coat rack": "mending",
+      "yoga": "stretch",
+      "clothes": "garment", //p156
+      "cloth": "linen", //only kanji in WK
+      // "towel ground": "market",
+      "oldersister": "elder sister",
+      "belt": "sash", //p161
+      "heaven": "heavens", //p164
+      // "stand": "stand up", // makes no difference for rtk-search
+      "chapter": "badge", //p166
+      "mohawk": "antique", //p167
+      "scent": "aroma",
+      "back": "stature", //p170
+      "point": "delicious",
+      "gun": "reclining",
+      "blackhole": "double back", //p175
+      "black hole": "double back",
+      "clown": "muzzle", //p178
+      // ---------------------------------- //
       "stick": "walking stick",
+      "small drop": "valentine",
       "drop": "drop of",
       "fins": "animal legs",
       "legs": "human legs", // or fenceposts
-      "table": "wind", //wind/weather vain
-      "prison": "bound up", //or bound up small
-      "horns": "horns",
       "lion": "straightened hook",
       "barb": "hook",
       "ground fins": "tool", // not ideal, this needs to be in combination
-      "narwhal": "by ones side",
       "knife": "saber",
-      "scarecrow": "wealth",
       "window": "mama",
       "triceratops": "little",
-      "big": "large",
       "cliff": "cliff",
       "flood": "flood",
-      "river": "stream",
       "tsunami": "water",
-      "original": "spring", // not perfect match, wk has cliff added
       "boil": "fire",
-      "head": "hood",
-      "roof": "house",
       "angel": "pole",
       "flower": "flower",
       "greenhouse": "graveyard",
@@ -62,34 +143,16 @@ $(function() {
       "animal": "pack of dogs",
       "slide dirt": "cow",
       "hat ground": "meeting",
-      "hat": "umbrella",
-      "scooter": "road",
-      "winter": "walking legs",
       "deathstar": "convoy",
       "death star": "convoy",
-      "forehead": "crown",
-      "lid table": "whirlwind",
-      "lid mouth": "tall",
-      "lid": "top hat",
       "dirt mouth": "lidded crock",
-      "viking": "schoolhouse",
       //"brush": "brush",
-      "ceremony": "arrow",
-      "drunkard": "fiesta",
-      "bar": "float",
-      "coatrack": "mending",
-      "coat rack": "mending",
-      "yoga": "stretch",
       "kick": "scarf",
       "spirit": "cloak",
       "cloud": "rising cloud",
       //"rain": "rain",
       //"ice": "ice",
       "heaven": "witch",
-      "mohawk": "antique",
-      "gun": "reclining",
-      "blackhole": "double back",
-      "clown": "muzzle",
       "guard": "devil",
       "alligator": "scorpion",
       "insect": "gnats",
@@ -101,8 +164,6 @@ $(function() {
       "mouth": "pent in",
       "canopy": "cave",
       "soul": "state of mind",
-      "small drop": "valentine",
-      "drop": "drop of",
       "fingers": "finger",
       "lantern": "two hands",
       "weapon": "missile",
@@ -170,7 +231,7 @@ $(function() {
       "grass": "owl",
       "football": "migrating ducks",
       "splinter": "talking cricket",
-      "village": "computer",
+      "village": "computer", // or ri, p80. but rtk-search has computer instead of ri
       //"tiger": "tiger",
       //"deer": "deer",
       "slice": "sign of the hog",
