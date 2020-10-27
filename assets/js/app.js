62110211c48950abed6f04cd4bb1fca41a7402aa
sschmidTU <s.schmid@phonicscore.com>
2020-10-28 00:36:24 +0100

    try fix gloom

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,7 +13,7 @@ $(function() {
     var query   = $('#search-query').val();
 
     if (query == "version") {
-      console.log("1.0.0.6");
+      console.log("1.0.0.7");
     }
 
     // mapping from WK radicals to RTK elements. (format of the values is comma separated, no spaces between values)
