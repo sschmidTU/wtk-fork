4debf5dd98f7c705e6f4744f7f9a92c2c8c3b064
sschmidTU <s.schmid@phonicscore.com>
2020-10-28 01:55:07 +0100

    fix 遥 element scooter/road missing

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,7 +13,7 @@ $(function() {
     var query   = $('#search-query').val();
 
     if (query == "version") {
-      console.log("1.0.0.9");
+      console.log("1.0.0.10");
     }
 
     // mapping from WK radicals to RTK elements. (format of the values is comma separated, no spaces between values)
