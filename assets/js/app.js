724b732c4673c88df7326cb6fb412afc5e362c6e
sschmidTU <s.schmid@phonicscore.com>
2020-10-30 19:30:43 +0100

    try remove empty blocks from kanji pages

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,7 +12,7 @@ $(function() {
     let query = $('#search-query').val();
 
     if (query === 'v' || query === 'version') {
-      console.log('wtk-search 1.0.5.8');
+      console.log('wtk-search 1.0.5.9');
     }
     query = query.toLowerCase(); // useful for mobile auto-correct. maybe check later if input like 'inX' is necessary
 
