99432361a7d7a066b076b5077e02e01f5678e5bb
sschmidTU <s.schmid@phonicscore.com>
2020-10-30 03:42:42 +0100

    fix wedding

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,7 +12,7 @@ $(function() {
     let query = $('#search-query').val();
 
     if (query === 'v' || query === 'version') {
-      console.log('wtk-search 1.0.5.4');
+      console.log('wtk-search 1.0.5.5');
     }
     query = query.toLowerCase(); // useful for mobile auto-correct. maybe check later if input like 'inX' is necessary
 
