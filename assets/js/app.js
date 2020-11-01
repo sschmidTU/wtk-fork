34a4fa85e2b1bda3af6132e7eddaa49adf0f7363
sschmidTU <s.schmid@phonicscore.com>
2020-11-01 02:17:00 +0100

    add not yet, end for extended WK

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,7 +12,7 @@ $(function() {
     let query = $('#search-query').val();
 
     if (query === 'v' || query === 'version') {
-      console.log('wtk-search 1.0.6.1');
+      console.log('wtk-search 1.0.6.2');
     }
     query = query.toLowerCase(); // useful for mobile auto-correct. maybe check later if input like 'inX' is necessary
 
@@ -41,6 +41,7 @@ $(function() {
       "mona lisa": "monalisa",
       "top hat": "tophat",
       "death star": "deathstar",
+      "not yet": "notyet", // officially jet in WK, but makes sense to distinguish from end/extremity (both jet in WK)
       "shamisen song": "shamisensong",
       "lip ring": "lipring",
     };
@@ -529,6 +530,8 @@ $(function() {
       "spider": "streetwalker",
       "horse": "team of horses",
       "jet": "not yet,extremity", // not yet is with the shorter top stroke, extremity with the longer (末 end kanji)
+      "notyet": "not yet", // officially jet in WK, but makes sense to distinguish from end/extremity
+      "end": "extremity", // officially jet in WK, but makes sense to distinguish from not yet
       // ---------------------------------- ^^ ------- //
       // ^ above checked with RTK physical edition, at least for WK radicals
       // ---- some WK radicals not existing in RTK ---- //
