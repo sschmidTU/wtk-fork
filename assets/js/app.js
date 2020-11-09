f9dde655c5874abf96d8f013e92d1b72097825ac
sschmidTU <s.schmid@phonicscore.com>
2020-11-09 18:50:48 +0100

    new urlParameter console: add wtk to window

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,7 +20,7 @@ class App {
     query = query.toLowerCase(); // useful for mobile auto-correct. maybe check later if input like 'inX' is necessary
 
     if (query == 'v' || query == 'version') {
-      this.log(this.LogLevels.Info, "1.2.0.4-ghpages_appjs-1.2.0.6");
+      this.log(this.LogLevels.Info, "1.2.0.4-ghpages_appjs-1.2.0.7");
     }
 
     const rtkMode = this.isRtkMode(); // used multiple times
@@ -361,6 +361,10 @@ class App {
     }
     if (params.compound === '1' || params.compound === 'true') {
       $(checkboxVocabQuery).click();
+    }
+    if (params.console === '1') {
+      window.wtk = this; // make wtk available in the console
+      // could also be window.wtksearch to avoid conflicts. but shorter/easier for now (debug)
     }
   }
 
