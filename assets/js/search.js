0e0752f5b8cbce7a14696a9185eeeaa45ef508a7
sschmidTU <s.schmid@phonicscore.com>
2020-11-17 23:22:13 +0100

    all wk keywords / kanji names searchable now
    add the wk main meaning to
    each kanji's keywordWK
    before indexing (lunr)

--- assets/js/search.js
+++ assets/js/search.js
@@ -15297,6 +15297,16 @@ var docs =
 },
 ];
 
+
+// add keywordWK info from wk_kanji data (wk_kanji_short_min.js)
+for (const doc of docs) {
+  if (wk_kanji[doc.kanji]) {
+    doc.keywordWK = wk_kanji[doc.kanji].meanings[0].meaning;
+  }
+}
+// TODO put all the keywordWK directly into the data so we don't have to do this every time.
+//   though this just takes 1ms
+
 // init lunr
 var idx = lunr(function () {
   this.field('keyword', 10);
@@ -15304,7 +15314,7 @@ var idx = lunr(function () {
   this.field('elements');
   this.field('elementsWK');
 });
-// add each document to be index
+// add each document to the index
 for(var index in docs) {
   idx.add(docs[index]);
 }
