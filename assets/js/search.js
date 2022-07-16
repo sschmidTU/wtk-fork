153f7e1e804b5c1e6169b969ec7a4b9a4dc8dbb9
sschmidTU <s.schmid@phonicscore.com>
2022-07-16 23:36:50 +0200

    search.js: always use elT (merge offline-always-us-elT-show-el-diff branch), adapt to new elementsDict changes

--- assets/js/search.js
+++ assets/js/search.js
@@ -5,87 +5,66 @@ for (const doc of docs) {
     // add keywordWK (kwWK) info from wk_kanji data (wk_kanji_short_min.js)
     doc.kwWK = wk_kanji[doc.kanji].meanings[0].meaning; // see wk_kanji_short_min.js
   }
-  if (!doc.el) {
-    //console.log("missing el for: " + doc.kanji); //debug
-    if (doc.elT) {
-      doc.elP = doc.elT.replaceAll("l(", "").replaceAll("t(","").replaceAll("o(","").replaceAll(")","");
+  if (doc.elT) {
+    // create doc.el (elements) from doc.elT (elementsTree)
+    doc.elP = removeStructure(doc.elT);
+    // TODO move this to a script that changes the .md files, instead of having visitors do this every time
+    let elP = doc.elP;
+    if (doc.elPx) {
+      elP += `, ${doc.elPx}`;
     }
-    if (doc.elP) {
-      // construct el (elements) from elP (elementsPure)
-      // TODO move this to a script that changes the .md files, instead of having visitors do this every time
-      let elP = doc.elP;
-      if (doc.elPx) {
-        elP += `, ${doc.elPx}`;
+    const elementsPure = elP.split(",");
+    let newElementsField = "";
+    const newElementsObject = {};
+    for (const element of elementsPure) {
+      let elementTrimmed = element.trim();
+      newElementsObject[elementTrimmed] = true; // save potentially with occurences
+      elementTrimmed = elementTrimmed.replaceAll(/[0-9]/g,""); // remove occurences
+
+      if (elementsDict[elementTrimmed]) {
+        const newSubElements = elementsDict[elementTrimmed].subElements;
+        for (const subElement of newSubElements) {
+          newElementsObject[subElement] = true;
+        }
+      } else {
+        // shouldn't happen; need to add element to elementsData.txt and node elementsDataToJson.js
+        console.log(`Error: element ${elementTrimmed} for kanji ${doc.kanji} wasn't found in elementsDict.`);
       }
-      const elementsPure = elP.split(",");
-      let newElementsField = "";
-      const newElementsObject = {}; // the elements already added. to avoid duplicates.
-      for (const element of elementsPure) {
-        let elementTrimmed = element.trim();
-
-        // check for number (of occurences) at the end, e.g. 'tree3' or 'jackhammer2' (WK).
-        //   copied code from wtksearch.js -> functionize? but 2 return values
-        let numberChar = elementTrimmed.charAt(elementTrimmed.length - 1);
-        let occurences = Number.parseInt(numberChar, 10);
-        if (isNaN(occurences)) {
-          numberChar = elementTrimmed.charAt(0);
-          occurences = Number.parseInt(numberChar, 10); // also check prefix number, e.g. '2tree'
-        }
-        if (!isNaN(occurences)) {
-          elementTrimmed = elementTrimmed.replace(numberChar, ''); // remove occurences for now, add again later
-        }
-        const occurencesString = occurences > 0 ? occurences.toString() : "";
-
-        if (elementsDict[elementTrimmed]) {
-          const newSubElements = elementsDict[elementTrimmed].elements.trim();
-          for (const subElement of newSubElements.split(",")) {
-            const subElementTrimmed = subElement?.trim();
-            if (!subElementTrimmed || subElementTrimmed.length === 0 || newElementsObject[subElementTrimmed]) {
-              continue;
-            }
-            if (newElementsField.length > 0) {
-              newElementsField += ", ";
-            }
-            newElementsField += subElementTrimmed + occurencesString;
-            newElementsObject[subElementTrimmed] = true;
-
-            const elementInfo = elementsDict[subElementTrimmed];  // TODO maybe take from element, not subElement, but then take out of subelements loop to avoid repetition
-            if (!elementInfo) {
-              // console.log("not in elementsdict: " + subElementTrimmed);
-              continue;
-            }
-            let elementsWKFromElement = elementInfo.elementsWK;
-            if (elementsWKFromElement) {
-              elementsWKFromElement = elementsWKFromElement.trim();
-              if (!doc.elWK) {
-                doc.elWK = elementsWKFromElement;
-              } else {
-                // check for each subelement if it's already in doc.elWK
-                const existingElWK = doc.elWK.split(",").map((a) => { return a.trim() });
-                const newElWKArray = elementsWKFromElement.split(",").map((a) => { return a.trim() });
-                for (const newElWK of newElWKArray) {
-                  if (existingElWK.includes(newElWK)) {
-                    // console.log("already exists: " + newElWK);
-                    continue;
-                  }
-                  doc.elWK += `, ${newElWK}`;
-                }
-              }
-              // console.log(`doc.elWK for ${doc.kanji}: ${doc.elWK}`);
-            }
-          }
-        } else {
-          // shouldn't happen; need to add element to elementsData.txt and node elementsDataToJson.js
-          console.log(`Error: element ${elementTrimmed} for kanji ${doc.kanji} wasn't found in elementsDict.`);
+    }
+    for (const subElement of Object.keys(newElementsObject)) {
+      if (newElementsField.length > 0) {
+        newElementsField += ", ";
+      }
+      newElementsField += subElement;
+    }
+    const compareOldAndNewElements = true;
+    if (compareOldAndNewElements && doc.el) {
+      //console.log("comparing old and new elements for " + doc.kanji);
+      // check old and new elements
+      const oldElementsObject = {};
+      const oldElements = doc.el.split(",");
+      for (const oldElement of oldElements) {
+        oldElementsObject[oldElement.trim()] = true;
+      }
+      //console.dir(oldElementsObject);
+      for (const oldElement of Object.keys(oldElementsObject)) {
+        if (!newElementsObject[oldElement]) {
+          console.log(`${doc.kanji}: new elements created from elT missing old element: ${oldElement}`);
         }
       }
-      doc.el = newElementsField;
-      //console.log("new elements field: " + newElementsField); //debug
-    } else {
-      // this shouldn't happen, having neither elements nor elementsPure
-      console.log("missing elements/elementsPure for " + doc.kanji); //debug
+      for (const newElement of Object.keys(newElementsObject)) {
+        if (!oldElementsObject[newElement]) {
+          console.log(`${doc.kanji}: old elements missing new element created from elT: ${newElement}`);
+        }
+      }
     }
-  }
+    doc.el = newElementsField;
+    //console.log("new elements field: " + newElementsField); //debug
+    if (!doc.el) {
+      // this shouldn't happen
+      console.log("missing elements for " + doc.kanji); //debug
+    }
+  } // end if doc.elT
 }
 // TODO put all the keywordWK directly into the data so we don't have to do this every time.
 //   though this just takes 1ms. Also it would increase the size of search.js by quite a bit.
@@ -100,4 +79,10 @@ var idx = lunr(function () {
 // add each document to be index
 for(var index in docs) {
   idx.add(docs[index]);
+}
+
+function removeStructure(elementsTreeString) {
+  return elementsTreeString.replaceAll(/[trlb][trlb]\(/g, "")
+      .replaceAll("l(", "").replaceAll("t(","").replaceAll("o(","").replaceAll("c(","")
+      .replaceAll(")","");
 }
