f2c58594599970e298a603cd9c9daea53c648725
sschmidTU <s.schmid@phonicscore.com>
2024-02-03 18:05:38 +0100

    data: 量 (quantity): add "amount" as convenience replacement

--- assets/js/wtksearch.js
+++ assets/js/wtksearch.js
@@ -1476,9 +1476,10 @@ class WTKSearch {
       "sleep": "sleep, lie down",
       "governmentoffice": "signature",
       "stretch": "stretch,lengthen", // 張 stretch radical is lengthen in RTK, but there's also 伸 stretch (expand in RTK, though it doesn't seem to be used in other kanji)
+      "sayhumbly": "say humbly,speaketh",
       // --- convenience replacements, false friends (close misses) ---
       "replace": "replace,substitute", // 替 is replace and 代 substitute, but my memory said 代 replace (which could be a common error)
-      "sayhumbly": "say humbly,speaketh",
+      "amount": "amount,quantity", // 量 quantity in WK, RTK. I once entered amount instead
     }
   }
 
