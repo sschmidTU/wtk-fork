8a4f25b4afb9f5aacdbce1cf29b21a811563ee43
sschmidTU <s.schmid@phonicscore.com>
2024-08-26 15:16:01 +0200

    tools: update uniqueKanji script to also count unique kanji per directory (across multiple files)

--- _tools/uniqueKanjiPerFile.mjs
+++ _tools/uniqueKanjiPerFile.mjs
@@ -14,29 +14,51 @@ async function init() {
         //"/../rtk1-v6/",
         //"/../rtk3-remain/"
     ];
-    
+    //getUniqueKanjiPerDirectoryPerFile(dirname, subDirectories);
+    getUniqueKanjiAcrossFiles(dirname, subDirectories);
+}
+
+/** Counts unique kanji per directory, so if there are 3 files with the same single kanji, total unique kanji = 1. */
+function getUniqueKanjiAcrossFiles(dirname, subDirectories) {
+    let uniqueKanji = {};
     for (const subDirectory of subDirectories) {
         console.log("directory: " + subDirectory)
         const dirname_nx = dirname + subDirectory;
         const files_nx = getFiles(dirname_nx);
-        getUniqueKanjiPerFile(dirname_nx, files_nx);
+        uniqueKanji = getUniqueKanjiPerFile(dirname_nx, files_nx, uniqueKanji);
+        console.log(`unique kanji across all files in ${subDirectory}: ${Object.keys(uniqueKanji).length}`);
+        uniqueKanji = {};
     }
 }
 
-function getUniqueKanjiPerFile(dirname, files) {
+function getUniqueKanjiPerDirectoryPerFile(dirname, subDirectories) {
+    for (const subDirectory of subDirectories) {
+        console.log("directory: " + subDirectory)
+        const dirname_nx = dirname + subDirectory;
+        const files_nx = getFiles(dirname_nx);
+        getUniqueKanjiPerFile(dirname_nx, files_nx, {});
+    }
+}
+
+function getUniqueKanjiPerFile(dirname, files, uniqueKanji = {}) {
+    // uniqueKanji can already contain found kanji, e.g. in other files.
     for (const file of files) {
+        let uniqueKanjiInFile = {};
         const fileString = FS.readFileSync(`${dirname}/${file}`).toString();
     
-        let uniqueKanji = {};
         for (const char of fileString) {
-            if (char >= '\u4e00' && char <= '\u9fff') {
+            if (char >= '\u4e00' && char <= '\u9fff') { // is kanji?
                 if (!uniqueKanji[char]) {
                     uniqueKanji[char] = true;
                 }
+                if (!uniqueKanjiInFile[char]) {
+                    uniqueKanjiInFile[char] = true;
+                }
             }
         }
-        console.log(`unique kanji in ${file}: ${Object.keys(uniqueKanji).length}`);
+        console.log(`unique kanji in ${file}: ${Object.keys(uniqueKanjiInFile).length}`);
     }
+    return uniqueKanji;
 }
 
 function getFiles(dirname) {
